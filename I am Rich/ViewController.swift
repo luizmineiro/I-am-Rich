74c4071d636cef7670b6eb40055c747c35534d4d
João Luiz Mineiro Alves <joao.alves@braza.com.br>
2025-07-17 18:26:12 -0300

    first commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dev on 17/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
